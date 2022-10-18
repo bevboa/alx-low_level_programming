a2d3e99b25790d4a96f1d1f20c494f372ea15615
bevboa <beverlyoware@gmail.com>
2022-10-18 08:03:02 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers of base ten
+ *
+ * Return:Always 0
+ */
+
+int main(void)
+
+{
+
+	int a;
+
+	for (a = 0; a < 10; a++)
+
+		printf("%d", a);
+
+	putchar('\n');
+
+	return (0);
+
+}
