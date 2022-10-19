eef7f5478698c7d3555ee7124310f3f03d83f570
bevboa <beverlyoware@gmail.com>
2022-10-19 06:12:15 -0700

    task 2-4

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	int ten;
+
+	char lc;
+
+	for (ten = 0; ten <= 9; ten++)
+
+	{
+
+		for (lc = 'a'; lc <= 'z'; lc++)
+
+			_putchar(lc);
+
+		_putchar('\n');
+
+	}
+
+}
