f8a3db3147b787d8209cf32b2f1d0b2234610170
bevboa <beverlyoware@gmail.com>
2022-11-19 11:25:36 -0800

    isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -16,28 +16,29 @@ int main(void)
 
 {
 
-	    int r;
+	int r;
 
 
 
-	        r = _isalpha('H');
+        r = _isalpha('H');
 
-		    _putchar(r + '0');
+        _putchar(r + '0');
 
-		        r = _isalpha('o');
+	r = _isalpha('o');
 
-			    _putchar(r + '0');
+	_putchar(r + '0');
 
-			        r = _isalpha(108);
+     	r = _isalpha(108);
 
-				    _putchar(r + '0');
+     	_putchar(r + '0');
 
-				        r = _isalpha(';');
+       	r = _isalpha(';');
 
-					    _putchar(r + '0');
+	_putchar(r + '0');
 
-					        _putchar('\n');
+	_putchar('\n');
 
-						    return (0);
+	return (0);
+
 
 }
