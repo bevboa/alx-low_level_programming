ffe8470b17276fdc9b84504b3a77c92f84ef4b02
bevboa <beverlyoware@gmail.com>
2022-11-23 07:42:43 -0800

    Static Libraries 0

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: string destination
+ * @src: string being copied
+ * @n: largest number of bytes to copy
+ * Return: address of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
